6705fabb43e3ced97371f648b008dca73c0c0ed2
nave91 <nave30041991@gmail.com>
2014-01-29 00:50:35 -0500

    after not-so-hards

--- pyy/310/2lab.py
+++ pyy/310/2lab.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import random
 
 ##Python code for lab2##
 ##Ques: Return a dictionary, converted to a string, 
@@ -13,4 +14,31 @@ def dictostr(dic):
     return string
 
 dic={1:'one',2:'two',3:'three'}
-print dictostr(dic)
+#print "####dictionary to string ",dictostr(dic)
+
+##Ques: Return one item in a list, selected at random. 
+##Hints: random.random and len.
+
+def retrandom(list):
+    return list[random.randint(0,len(list)-1)]
+
+list = [7,8,9,34]
+#print retrandom(list)
+
+##Ques: Return n items in a list, selected at random, 
+##      and if the same thing gets selected twice 
+##      you reject it and try again.
+
+def retnitems(list,n):
+    s = []
+    for i in range(n):
+        temp = list[random.randint(0,len(list)-1)]
+        if temp not in s:
+            s.append(temp)
+            i -= 1
+    return s
+
+
+list = [i for i in range(30)]
+n = 3
+print retnitems(list,n)
